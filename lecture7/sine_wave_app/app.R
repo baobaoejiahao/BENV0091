8d6585f73abd7206143836f074dc0f6af954906d
Patrick de Mars <pwdemars@gmail.com>
2021-11-24 12:17:53 +0000

    Added my_app

--- lecture7/sine_wave_app/app.R
+++ lecture7/sine_wave_app/app.R
@@ -1,16 +1,25 @@
 library(shiny)
+library(tidyverse)
 
 # Front-end (UI)
 ui <- fluidPage(
-  sliderInput(inputId = "num", 
+  titlePanel("My Sine Wave"),
+  p("This app studies the function y = sin(cx)"),
+  hr(),
+  sidebarLayout(
+    sidebarPanel(sliderInput(inputId = "num", 
               label = "Choose a number", 
-              value = 2, min = 0.1, max = 10),
-  plotOutput("hist")
+              value = 2, min = 0.1, max = 10)),
+    mainPanel(plotOutput("wave"))
+  )
 )
 
 # R Code (server)
 server <- function(input, output) {
-  output$hist <- renderPlot({
+  
+  value <- reactive(input$num)
+  
+  output$wave <- renderPlot({
     data <- tibble(x = seq(0, 2*pi, 0.01),
                    y = sin(input$num * x))
     ggplot(data, aes(x = x, y = y)) + geom_line()
@@ -19,6 +28,8 @@ server <- function(input, output) {
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+
 
 
 
