2d0824dbb1c17850661cecc09bf045fd24837893
Patrick de Mars <pwdemars@gmail.com>
2021-11-24 18:42:35 +0000

    Added year to title

--- lecture7/electric_insights_app/app.R
+++ lecture7/electric_insights_app/app.R
@@ -13,7 +13,7 @@ source('electric_insights_plot.R')
 
 # Front-end (UI)
 ui <- fluidPage(
-  titlePanel("GB Fuel Mix by Week"),
+  titlePanel("GB Fuel Mix by Week (2020)"),
   sliderInput(inputId = "week_no", 
               label = "Choose a week number", 
               value = 1, min = 1, max = 52, step = 1),
